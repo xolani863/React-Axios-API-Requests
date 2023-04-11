2219a714616ab7d868f1fc0497c796afed04f02e
xolani863 <mhlangaxlolani21@gmail.com>
2023-04-11 12:52:53 +0200

    Create Operation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Create from './components/create/create';
+import Read from './components/read/read';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Update from './components/update/update';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="main">
+        <div>
+          <h3>React Crud Operations</h3>
+        </div>
+
+        <div>
+          <Route exact path='/' component={Create} />
+        </div>
+
+        <div style={{ marginTop: 20 }}>
+          <Route exact path='/read' component={Read} />
+        </div>
+
+        <Route path='/update' component={Update} />
+        
+      </div>
+    </Router>
   );
 }
 
