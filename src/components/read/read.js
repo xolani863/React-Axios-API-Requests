28563ef12404b31cacb751fd94007aabebe9be0d
xolani863 <mhlangaxlolani21@gmail.com>
2023-04-11 13:02:39 +0200

    Create Operation

--- src/components/read/read.js
+++ src/components/read/read.js
@@ -1,75 +1,75 @@
-import React, { useEffect, useState } from 'react';
-import { Table, Button } from 'semantic-ui-react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+// import React, { useEffect, useState } from 'react';
+// import { Table, Button } from 'semantic-ui-react';
+// import axios from 'axios';
+// import { Link } from 'react-router-dom';
 
-export default function Read() {
-    const [apiData, setApiData] = useState([]);
-    useEffect(() => {
-        axios.get(`https://643523bb83a30bc9ad58dc0e.mockapi.io/Crud`)
-            .then((getData) => {
-                setApiData(getData.data);
-            })
-    }, [])
+// export default function Read() {
+//     const [apiData, setApiData] = useState([]);
+//     useEffect(() => {
+//         axios.get(`https://643523bb83a30bc9ad58dc0e.mockapi.io/Crud`)
+//             .then((getData) => {
+//                 setApiData(getData.data);
+//             })
+//     }, [])
 
-    const setData = (id, firstName, lastName) => {
-        localStorage.setItem('ID', id)
-        localStorage.setItem('firstName', firstName)
-        localStorage.setItem('lastName', lastName)
-    }
+//     const setData = (id, firstName, lastName) => {
+//         localStorage.setItem('ID', id)
+//         localStorage.setItem('firstName', firstName)
+//         localStorage.setItem('lastName', lastName)
+//     }
 
-    const getData = () => {
-        axios.get(`https://643523bb83a30bc9ad58dc0e.mockapi.io/Crud`)
-            .then((getData) => {
-                setApiData(getData.data);
-            })
-    }
+//     const getData = () => {
+//         axios.get(`https://643523bb83a30bc9ad58dc0e.mockapi.io/Crud`)
+//             .then((getData) => {
+//                 setApiData(getData.data);
+//             })
+//     }
 
-    const onDelete = (id) => {
-        axios.delete(`https://643523bb83a30bc9ad58dc0e.mockapi.io/${id}`)
-        .then(() => {
-            getData();
-        })
-    }
+//     const onDelete = (id) => {
+//         axios.delete(`https://643523bb83a30bc9ad58dc0e.mockapi.io/${id}`)
+//         .then(() => {
+//             getData();
+//         })
+//     }
 
-    return (
-        <div>
-            <Table celled>
-                <Table.Header>
-                    <Table.Row>
-                        <Table.HeaderCell>ID</Table.HeaderCell>
-                        <Table.HeaderCell>First Name</Table.HeaderCell>
-                        <Table.HeaderCell>Last Name</Table.HeaderCell>
-                        <Table.HeaderCell>Update</Table.HeaderCell>
-                        <Table.HeaderCell>Delete</Table.HeaderCell>
-                    </Table.Row>
-                </Table.Header>
+//     return (
+//         <div>
+//             <Table celled>
+//                 <Table.Header>
+//                     <Table.Row>
+//                         <Table.HeaderCell>ID</Table.HeaderCell>
+//                         <Table.HeaderCell>First Name</Table.HeaderCell>
+//                         <Table.HeaderCell>Last Name</Table.HeaderCell>
+//                         <Table.HeaderCell>Update</Table.HeaderCell>
+//                         <Table.HeaderCell>Delete</Table.HeaderCell>
+//                     </Table.Row>
+//                 </Table.Header>
 
-                <Table.Body>
-                    {apiData.map((data) => {
-                        return (
-                            <Table.Row>
-                                <Table.Cell>{data.id}</Table.Cell>
-                                <Table.Cell>{data.firstName}</Table.Cell>
-                                <Table.Cell>{data.lastName}</Table.Cell>
-                                <Table.Cell>
-                                    <Link to='/update'>
-                                        <Button
-                                            color="green"
-                                            onClick={() => setData(data.id, data.firstName, data.lastName)}>
-                                            Update
-                                        </Button>
-                                    </Link>
-                                </Table.Cell>
-                                <Table.Cell>
-                                    <Button color="red" onClick={() => onDelete(data.id)}>Delete</Button>
-                                </Table.Cell>
-                            </Table.Row>
-                        )
-                    })}
+//                 <Table.Body>
+//                     {apiData.map((data) => {
+//                         return (
+//                             <Table.Row>
+//                                 <Table.Cell>{data.id}</Table.Cell>
+//                                 <Table.Cell>{data.firstName}</Table.Cell>
+//                                 <Table.Cell>{data.lastName}</Table.Cell>
+//                                 <Table.Cell>
+//                                     <Link to='/update'>
+//                                         <Button
+//                                             color="green"
+//                                             onClick={() => setData(data.id, data.firstName, data.lastName)}>
+//                                             Update
+//                                         </Button>
+//                                     </Link>
+//                                 </Table.Cell>
+//                                 <Table.Cell>
+//                                     <Button color="red" onClick={() => onDelete(data.id)}>Delete</Button>
+//                                 </Table.Cell>
+//                             </Table.Row>
+//                         )
+//                     })}
 
-                </Table.Body>
-            </Table>
-        </div>
-    )
-}
+//                 </Table.Body>
+//             </Table>
+//         </div>
+//     )
+// }
